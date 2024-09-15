ec8f1e5753fec8522d560986a5040e9a71c5a0e2
RahimeUlusoy <rulusoy.2516@gmail.com>
2024-09-15 15:22:11 +0300

    Navbar.scss oluşturuldu. stil tanımları yapıldı. index.scss import edildi. logo eklendi. nav listesi oluşturuldu.

---
+++ src/componenets/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.scss';
+import logo from '../../logo.png';
+
+export default function Navbar() {
+  return (
+    <nav className='Navbar'>
+<img src={logo} alt='city tours logo'/>
+<ul className='nav-links'>
+    <li>
+        <a href='/' className='nav-link'>
+        home
+        </a>
+    </li>
+
+    <li>
+        <a href='/' className='nav-link'>
+        about
+        </a>
+    </li>
+
+    <li>
+        <a href='/' className='nav-link active'>
+        tours
+        </a>
+    </li>
+
+
+</ul>
+    </nav>
+  );
+}
