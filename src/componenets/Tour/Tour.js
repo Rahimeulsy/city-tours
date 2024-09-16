e46e6c60bc6643dc793b12f4f6653d4fb0c6ebc3
RahimeUlusoy <rulusoy.2516@gmail.com>
2024-09-16 10:23:38 +0300

    Düzenleme yapıldı.

--- src/componenets/Tour/Tour.js
+++ src/componenets/Tour/Tour.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import "./Tour.scss";
 export default class Tour extends Component {
   state = {
-    showInfo: false,
+    showInfo: false, //Bu, turun ek bilgileri (info) gösterilip gösterilmeyeceğini kontrol eder.
   };
-  handleInfo=()=>{
+  handleInfo=()=>{ //handleInfo adında bir fonksiyon tanımlanmış. Bu fonksiyon, showInfo durumunu tersine çevirir (true ise false, false ise true yapar).
     this.setState({
         showInfo: !this.state.showInfo
     });
   };
   render() {
     const { id, city, img, name, info } = this.props.tour;
-    const {removeTour}=this.props;
-    return (
+    const {removeTour}=this.props; //removeTour, yine dışarıdan (üst bileşenden) gelen bir fonksiyondur. Bu fonksiyon bir turu listeden silmek için kullanılacak.
+    return ( //article etiketi, her bir turu temsil eden kapsayıcı elementtir.
       <article className="tour">
         <div className="img-container">
           <img src={img} alt="" />
@@ -26,12 +26,13 @@ export default class Tour extends Component {
           <h5>
             info{""}
             <span onClick={this.handleInfo}>
-              <i className="fas fa-caret-square-down" />
+              <i className="fas fa-caret-square-down" /> 
             </span>
           </h5>
           {this.state.showInfo && <p>{info}</p>}
         </div>
       </article> //fas fa-window-close yazı tipi bir simge üzerine alınır.
+      //aşağı bakan bir ok simgesi olan kare şeklini ifade eder.
     );
   }
 }
--- src/componenets/TourList/index.js
+++ src/componenets/TourList/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./TourList.scss";
 import Tour from "../Tour/Tour";
-import { tourData } from "../tourData";
+import { tourData } from "../tourData"; //Tour bileşeni ve tourData (tur verisi) da ilgili dosyalardan içe aktarılıyor.
 
 export default class TourList extends Component {
   state = {
     tours: tourData,
   };
-  removeTour = (id) => {
-    const { tours } = this.state;
-    const sortedTours = tours.filter((tour) => tour.id !== id);
+  removeTour = (id) => { //removeTour adında bir fonksiyon tanımlanmış. Bu fonksiyon, verilen id değerine göre bir turu listeden çıkarmak için kullanılıyor.
+    const { tours } = this.state; //tours değişkeni state'den alınarak, mevcut turlar dizisi kopyalanıyor.
+    const sortedTours = tours.filter((tour) => tour.id !== id);//filter metodu ile id'si verilen tur, turlar dizisinden çıkarılıyor (tour.id !== id ifadesiyle).
     this.setState({
         tours: sortedTours,
     });
@@ -18,10 +18,10 @@ export default class TourList extends Component {
     const { tours } = this.state;
     return (
       <section className="tourlist">
-        {tours.map((tour) => {
-          return <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />;
+        {tours.map((tour) => { //map fonksiyonu ile tours dizisi üzerinden dönülerek, her bir tur için bir Tour bileşeni render ediliyor.
+          return <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />;//key={tour.id}: React'e her bir öğenin benzersiz olduğunu söylemek için kullanılıyor, React'in bu öğeleri takip edebilmesi için id benzersiz olmalıdır.
         })}
-      </section>
-    );
+      </section>//tour={tour}: Her Tour bileşenine ilgili turun tüm bilgilerini (img, name, info vb.) props olarak geçiriyor.
+    );//removeTour={this.removeTour}: Her Tour bileşenine removeTour fonksiyonunu props olarak geçiyor. Bu sayede, her bir turu silmek için removeTour fonksiyonu kullanılabilir.
   }
 }
